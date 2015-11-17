a3cf946f370cfd14990630e4d83bc4dd6c84c896
jeremy <jeremymartinez11@gmail.com>
2015-11-17 01:12:04 -0800

    modularized

--- node.py
+++ node.py
@@ -4,6 +4,7 @@
 
 import sys
 from readrouters import *
+from router import *
 import socket
 
 def listen(port, links):
--- router.py
+++ router.py
@@ -4,20 +4,34 @@
 
 import sys
 from readrouters import *
+from node import *
 import socket
 
 def broadcast():
 	#patient side of UDP communication
-	context = zmq.Context()
-	socket = context.socket(zmq.REQ)
-	socket.connect("tcp://127.0.0.1:50001")
 
-	for i in range(100):
-		msg = "msg %s" % i
-		socket.send(msg)
-		print "Sending", msg
-		msg_in = socket.recv()	
-	pass
+    if __name__ == '__main__':
+        if len(sys.argv) < 3:
+            exit("missing arguments")
+           
+
+    table = readrouters(sys.argv[1])
+    print table
+
+    links = readlinks(sys.argv[1], sys.argv[2])
+    i = 0
+    
+    sock = {}
+    for link in links:
+        data = data + "{0} {1} {2} {3}\n".format(link, links[link].cost, links[link].locallink, links[link].remotelink)
+    
+    print 'Start Broadcast'
+     
+    for i in table:
+         sock[i] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+         server_address = (table[i].host, table[i].baseport)
+         sock.sendto(data, server_address)
+
 
 if __name__ == '__main__':
 	table = readrouters(sys.argv[1])
