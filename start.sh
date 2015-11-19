b1512a36adceb3abf6d27ab1078c93d867095756
jeremy <jeremymartinez11@gmail.com>
2015-11-18 16:53:11 -0800

    output added

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+python node.py test1 A > OUTPUT.txt &
+python node.py test1 B > OUTPUT.txt &
+python node.py test1 C > OUTPUT.txt &
+python node.py test1 D > OUTPUT.txt &
+python node.py test1 E > OUTPUT.txt &
