05cb5e79eae4b4399d41110b2df9c19058c81b7a
jeremy <jeremymartinez11@gmail.com>
2015-11-18 16:39:35 -0800

    donezo

--- node.py
+++ node.py
@@ -7,8 +7,6 @@ import socket
 import select
 import Queue
 
-connected_sockets = []
-
 def listen(table, links, server):
 	inputs = [server]
 	outputs = []
@@ -23,47 +21,72 @@ def listen(table, links, server):
 				inputs.append(connection)
 
 			else:
+				# recv data up to 1024 bytes
 				data = s.recv(1024)
-				print data
 				if data == 'P':
 					printTable(links)
+				elif data[0] == 'L':
+					# update links locally and then broadcast that to all other nodes it is linked to
+					links = changeLink(links, data)
+					broadcast(connected_sockets, links) # imported from router.py
 				else:
+					# call update; if links WAS updated, broadcast new changes to linked nodes
 					updated, links = update(data, links)
 					if updated:
-						broadcast(connected_sockets, links)
+						broadcast(connected_sockets, links) # imported from router.py
+				# remove inputs so next select call does not read it again
 				inputs.remove(s)
+				# close socket that select intercepted and that we were reading from
 				s.close()
 
 def printTable(links):
 	print_links(links, status="current links")
 
+def changeLink(links, data):
+	data = data.split(' ')
+	# only update cost of link to what changelink sent us
+	links[data[1]].cost = data[2]
+	return links
+
+# update returns a tuple (boolean, class<LinkInfo>)
+# boolean says whether <links> was changed at all in this function
+# <links> is updated and returned so that it can be updated in the calling function (listen())
 def update(data, links):
 	updates = False
 
 	rows = data.split('\n')
+	# read first character in packet to see who sent the packet
 	from_node = rows[0]
 
+	# set the node the packet came from to the node in the links class
 	hop_off_node = links[from_node]
 
+	# remove leading character so that we can loop through <hop_off_node>'s links for comparison
 	rows = rows[1:]
-	rows = filter(None, rows)
 
-	old = links.copy()
+	old = links.copy() # deep copy for comparison later between old links and new links
 
+	# loop links
 	for row in rows:
+		# split string by spaces in order to pick up multi-digit cost's
 		tmp = row.split(' ')
+		# if we're looking at ourselves, pass
 		if this_router == tmp[0]:
 			pass
+		# if we already have a cost for reaching this node
 		elif tmp[0] in links:
-			if int(tmp[1])+hop_off_node.cost < links[tmp[0]].cost:
-				links[tmp[0]].cost = int(tmp[1])+hop_off_node.cost
-				links[tmp[0]].locallink = hop_off_node.locallink
+			# if the combined cost of the connecting link and the <hop_off_node>'s link is less then our current cost, update our link
+			if int(tmp[1])+int(hop_off_node.cost) < links[tmp[0]].cost:
+				links[tmp[0]].cost = int(tmp[1])+int(hop_off_node.cost)
+				links[tmp[0]].locallink = int(hop_off_node.locallink)
 				links[tmp[0]].remotelink = int(tmp[2]) #next hop locallink
 				updates = True
+		# if we currently cannot reach this node, create a link for it
 		else:
-			links[tmp[0]] = LinkInfo(int(tmp[1])+hop_off_node.cost, hop_off_node.locallink, tmp[2])
+			links[tmp[0]] = LinkInfo(int(tmp[1])+int(hop_off_node.cost), int(hop_off_node.locallink), int(tmp[2]))
 			updates = True
 
+	# compare deep copy from before to <links> here. If they're different, print the old, imported, and new <links>
 	if updates:
 		status = "old links: %s" % this_router
 		print_links(links, from_node, status=status, rows=rows)
@@ -71,6 +94,7 @@ def update(data, links):
 
 	return updates, links
 
+# print old links, the links being sent to us, and the new links as a result of analyzing those
 def print_links(links, from_node=None, rows=None, status=""):
 	print status
 	if rows:
@@ -86,21 +110,21 @@ def print_links(links, from_node=None, rows=None, status=""):
 		print '---------------------------'
 	print '---------------------------'
 
+# set socket for the server to listen on
 def set_sockets(table, links):
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server.setblocking(0)
 
-	# Bind the socket to the port
 	server_address = ('localhost', table[sys.argv[2]].baseport)
 	print >>sys.stderr, 'starting up on %s port %s' % server_address
 	server.bind(server_address)
 
-	# Listen for incoming connections
 	server.listen(5)
 
 	return server
 
+#find all nodes we are connected to initially
 def fill_connected_sockets(table, links):
 	connected = []
 	for t in table:
@@ -109,6 +133,7 @@ def fill_connected_sockets(table, links):
 	return connected
 
 this_router = sys.argv[2]
+connected_sockets = []
 
 if __name__ == '__main__':
 	table = readrouters(sys.argv[1])
--- router.py
+++ router.py
@@ -9,29 +9,39 @@ import socket
 
 this_router = sys.argv[2]
 
+# broadcast sends to all connected links their own information on their connectivity and cost inside the network
+# when called from __main__:
+#   this only broadcasts the links found in <testdir>/*.cfg
+# when called from node.py:
+#   this will broadcast the current links (initial and updated) too all of its connected links
 def broadcast(connected_sockets, links, table=None):
   message = this_router + '\n'
   for link in links:
     message = message + link + ' ' + str(links[link]) + '\n'
-  message = message
+  message = message[:len(message)-1]
+  # example broadcast might look like: "A\nB 7 1 1\nE 1 2 2"
 
+  # connect and send too all connected nodes and watch the magic unfold!
   for conn in connected_sockets:
     sockets = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sockets.connect(conn)
     sockets.send(message)
 
+# __main__ is invoked when we initially broadcast from a specific node starting the chain reaction of communication between all nodes
 if __name__ == '__main__':
   offset = 1
   if sys.argv[1] == '-p':
     offset = offset + 1
-    #poison bitch
+    #poison
     pass
   dirname = sys.argv[offset]
   router = sys.argv[offset+1]
 
+  # imported from readrouters.py
   table = readrouters(dirname)
   links = readlinks(dirname, router)
 
+  # imported from node.py
   connected_sockets = fill_connected_sockets(table, links)
 
   broadcast(connected_sockets, links)
